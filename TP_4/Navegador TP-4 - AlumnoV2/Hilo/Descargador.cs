f68ed2281fe8e59a4c06244549325b6ccab790c1
francocampaiola <francocampaiola@hotmail.com>
2017-06-16 11:42:34 -0300

    TP4 FINAL

---
+++ TP_4/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public delegate void Progress(int progress);
+        public delegate void DownloadComplete(string codigo);
+
+        public event Progress progreso;
+        public event DownloadComplete descargaCompleta;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progreso(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.html = e.Result;
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            this.descargaCompleta(this.html);
+        }
+    }
+}
---
+++ TP_4/Navegador TP-4 - AlumnoV2/Navegador/frmWebBrowser.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using Hilo;
+
+namespace Navegador
+{
+    public partial class frmWebBrowser : Form
+    {
+        private const string ESCRIBA_AQUI = "Escriba aquí...";
+        Archivos.Texto archivos;
+
+        public frmWebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void frmWebBrowser_Load(object sender, EventArgs e)
+        {
+            this.txtUrl.SelectionStart = 0;  //This keeps the text
+            this.txtUrl.SelectionLength = 0; //from being highlighted
+            this.txtUrl.ForeColor = Color.Gray;
+            this.txtUrl.Text = frmWebBrowser.ESCRIBA_AQUI;
+
+            archivos = new Archivos.Texto(frmHistorial.ARCHIVO_HISTORIAL);
+        }
+
+        #region "Escriba aquí..."
+        private void txtUrl_MouseMove(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.IBeam; //Without this the mouse pointer shows busy
+        }
+
+        private void txtUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (this.txtUrl.Text.Equals(frmWebBrowser.ESCRIBA_AQUI) == true)
+            {
+                this.txtUrl.Text = "";
+                this.txtUrl.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (this.txtUrl.Text.Equals(null) == true || this.txtUrl.Text.Equals("") == true)
+            {
+                this.txtUrl.Text = frmWebBrowser.ESCRIBA_AQUI;
+                this.txtUrl.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtUrl_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.txtUrl.SelectAll();
+        }
+        #endregion
+
+        delegate void ProgresoDescargaCallback(int progreso);
+        private void ProgresoDescarga(int progreso)
+        {
+            if (statusStrip.InvokeRequired)
+            {
+                ProgresoDescargaCallback d = new ProgresoDescargaCallback(ProgresoDescarga);
+                this.Invoke(d, new object[] { progreso });
+            }
+            else
+            {
+                tspbProgreso.Value = progreso;
+            }
+        }
+        delegate void FinDescargaCallback(string html);
+        private void FinDescarga(string html)
+        {
+            if (rtxtHtmlCode.InvokeRequired)
+            {
+                FinDescargaCallback d = new FinDescargaCallback(FinDescarga);
+                this.Invoke(d, new object[] { html });
+            }
+            else
+            {
+                rtxtHtmlCode.Text = html;
+            }
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            Uri direccion;
+
+            if (!this.txtUrl.Text.StartsWith("http://"))
+            {
+                this.txtUrl.Text = "http://" + this.txtUrl.Text;
+            }
+
+            direccion = new Uri(this.txtUrl.Text);
+
+            if (direccion != null)
+            {
+                Descargador dw = new Descargador(direccion);
+                dw.progreso += new Descargador.Progress(this.ProgresoDescarga);
+                dw.descargaCompleta += new Descargador.DownloadComplete(this.FinDescarga);
+
+                Thread h = new Thread(dw.IniciarDescarga);
+                h.Start();
+                this.archivos.guardar(this.txtUrl.Text);
+            }
+        }
+
+        private void mostrarHistorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new frmHistorial().ShowDialog();
+        }
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        { }
+
+        private void btnIr_Click_1(object sender, EventArgs e)
+        { 
+        
+        }
+
+        private void tspbProgreso_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUrl_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIr_Click_2(object sender, EventArgs e)
+        {
+            Uri direccion;
+
+            if (!this.txtUrl.Text.StartsWith("http://"))
+            {
+                this.txtUrl.Text = "http://" + this.txtUrl.Text;
+            }
+
+            direccion = new Uri(this.txtUrl.Text);
+
+            if (direccion != null)
+            {
+                Descargador dw = new Descargador(direccion);
+                dw.progreso += new Descargador.Progress(this.ProgresoDescarga);
+                dw.descargaCompleta += new Descargador.DownloadComplete(this.FinDescarga);
+
+                Thread h = new Thread(dw.IniciarDescarga);
+                h.Start();
+                this.archivos.guardar(this.txtUrl.Text);
+            }
+        }
+
+        private void mostrarTodoElHistorialToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void mostrarTodoElHistorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new frmHistorial().ShowDialog();
+        }
+    }
+}
